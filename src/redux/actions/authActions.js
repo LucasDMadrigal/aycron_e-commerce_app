15671888f8dcbb2565fbdb8572a49ec7afdb975b
Lucas Madrigal <lucasmadrigalb@gmail.com>
2025-05-12 18:28:25 -0300

    store iniciado

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -5,9 +5,11 @@ export const login = createAction('LOGIN', (user) => {
     const clearUser = {
         name: user.name,
         lastName: user.lastName,
+        token: user.token,
+        expiresIn: user.expiresIn,
+        loggedIn: true,
+        rol: user.rol
     }
 
-    return {payload: {
-        clearUser,
-    }}
+    return {payload: clearUser}
 })
--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -1,29 +1,34 @@
-import { createReducer } from '@reduxjs/toolkit'
-import { login } from '../actions/authActions'
+import { createReducer } from "@reduxjs/toolkit";
+import { login } from "../actions/authActions";
 
 const initialState = {
-    loggedIn: false,
-    token:"",
-    expiresIn: 0,
-    user: {
-        name: "",
-        lastName: "",
-        email: "",
-    }
-
-}
+  user: {
+    name: "",
+    lastName: "",
+    rol: "",
+  },
+  token: "",
+  expiresIn: "",
+  loggedIn: false,
+};
 
 const authReducer = createReducer(initialState, (builder) => {
-    builder.addCase(login, (state, action) => {
-        return {
-            ...state,
-            loggedIn: true,
-            token: action.payload.token,
-            expiresIn: action.payload.expiresIn,
-            user: action.payload.user
-        }
+  builder.addCase(login, (state, action) => {
+    const newState = {
+      ...state,
+      loggedIn: true,
+      token: action.payload.token,
+      expiresIn: action.payload.expiresIn,
+      user: action.payload.user,
+    };
 
-    })
-})
+    localStorage.setItem("user", JSON.stringify(newState));
+    return newState;
+  })
+  .addCase('LOGOUT', () => {
+    localStorage.removeItem('user');
+    return initialState;
+  })
+});
 
-export default authReducer
+export default authReducer;
