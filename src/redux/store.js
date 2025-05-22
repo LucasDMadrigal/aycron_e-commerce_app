3f0ae1d0a62023a1bbdcf678f4f965cc6e4f6c77
Lucas Madrigal <lucasmadrigalb@gmail.com>
2025-05-21 23:32:16 -0300

    correcciones en store redux2

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./reducers/authReducer";
 import cartReducer from "./reducers/cartReducer";
+const persistedCart = JSON.parse(localStorage.getItem('cart')) || [];
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     cart: cartReducer,
   },
+  preloadedState: {
+    cart: persistedCart
+  }
 });
